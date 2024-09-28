c97aba0adeed9b8680b3b549eecbcf83b7e141a8
OfficerZhou <2110646202@qq.com>
2024-09-28 17:22:22 +0800

    Pointers2

--- Learning/AdvancedPointers.c
+++ Learning/AdvancedPointers.c
@@ -1,11 +1,19 @@
 #include<stdio.h>
-void test(int arr[])
-	{
-	int sz = sizeof(arr) / sizeof(arr[0]);//64位-地址大小为8bit/int类型数据4bit=2
-	printf("%d", sz);
-}
-int main()
-{
-	char arr[10] = { 0 };
-	test(arr);
-}
+//void test(int arr[])
+//	{
+//	int sz = sizeof(arr) / sizeof(arr[0]);//64位-地址大小为8bit/int类型数据4bit=2
+//	printf("%d", sz);
+//}
+//int main()
+//{
+//	char arr[10] = { 0 };
+//	test(arr);
+//}
+////字符指针
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	char* p = arr;
+//	printf("%s\n", arr);
+//	printf("%s\n", p);
+//}
